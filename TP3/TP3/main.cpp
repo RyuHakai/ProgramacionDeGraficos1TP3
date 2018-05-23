bfb87212b354722c795ddfd83e26e6623f0afaa4
Unknown <lean1998@hotmail.com>
2018-05-23 12:44:25 -0300

    se mueve

--- TP3/TP3/main.cpp
+++ TP3/TP3/main.cpp
@@ -3,21 +3,47 @@
 #include "allegro5/allegro_image.h"
 #include "allegro5/allegro_native_dialog.h"
 
-int main(int argc, char **argv) {
+const float FPS = 60;
+const int SCREEN_W = 640;
+const int SCREEN_H = 480;
+const int BOUNCER_SIZE = 32;
+enum MYKEYS {
+	KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
+};
 
+int main(int argc, char **argv)
+{
 	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+	ALLEGRO_TIMER *timer = NULL;
 	ALLEGRO_BITMAP  *bloque = NULL;
 	ALLEGRO_BITMAP  *player = NULL;
-
+	float player_x = SCREEN_W / 2.0 - BOUNCER_SIZE / 2.0;
+	float player_y = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;
+	bool key[4] = { false, false, false, false };
+	bool redraw = true;
+	bool doexit = false;
 
 	if (!al_init()) {
 		fprintf(stderr, "failed to initialize allegro!\n");
 		return -1;
 	}
 
-	display = al_create_display(800, 600);
+	if (!al_install_keyboard()) {
+		fprintf(stderr, "failed to initialize the keyboard!\n");
+		return -1;
+	}
+
+	timer = al_create_timer(1.0 / FPS);
+	if (!timer) {
+		fprintf(stderr, "failed to create timer!\n");
+		return -1;
+	}
+
+	display = al_create_display(SCREEN_W, SCREEN_H);
 	if (!display) {
 		fprintf(stderr, "failed to create display!\n");
+		al_destroy_timer(timer);
 		return -1;
 	}
 
@@ -26,6 +52,7 @@ int main(int argc, char **argv) {
 			NULL, ALLEGRO_MESSAGEBOX_ERROR);
 		return 0;
 	}
+
 
 	bloque = al_load_bitmap("bloque.jpg");
 
@@ -45,19 +72,118 @@ int main(int argc, char **argv) {
 		return 0;
 	}
 
-	al_clear_to_color(al_map_rgb(0, 0, 0));
+	
 
-	al_draw_bitmap(bloque, 700, 60, 0);
-	al_draw_bitmap(player, 200, 60, 0);
+	al_clear_to_color(al_map_rgb(255, 0, 255));
+
+	al_set_target_bitmap(al_get_backbuffer(display));
+
+	event_queue = al_create_event_queue();
+	if (!event_queue) {
+		fprintf(stderr, "failed to create event_queue!\n");
+		al_destroy_bitmap(player);
+		al_destroy_display(display);
+		al_destroy_timer(timer);
+		return -1;
+	}
+
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+
+	al_register_event_source(event_queue, al_get_keyboard_event_source());
+
+	al_clear_to_color(al_map_rgb(0, 0, 0));
 
 	al_flip_display();
 
-	al_rest(1);
+	al_start_timer(timer);
 
-	al_destroy_display(display);
+	while (!doexit)
+	{
+		ALLEGRO_EVENT ev;
+		al_wait_for_event(event_queue, &ev);
 
-	al_destroy_bitmap(bloque);
+		if (ev.type == ALLEGRO_EVENT_TIMER) {
+			if (key[KEY_UP] && player_y >= 4.0) {
+				player_y -= 4.0;
+			}
+
+			if (key[KEY_DOWN] && player_y <= SCREEN_H - BOUNCER_SIZE - 4.0) {
+				player_y += 4.0;
+			}
+
+			if (key[KEY_LEFT] && player_x >= 4.0) {
+				player_x -= 4.0;
+			}
+
+			if (key[KEY_RIGHT] && player_x <= SCREEN_W - BOUNCER_SIZE - 4.0) {
+				player_x += 4.0;
+			}
+
+			redraw = true;
+		}
+		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			break;
+		}
+		else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+			switch (ev.keyboard.keycode) {
+			case ALLEGRO_KEY_UP:
+				key[KEY_UP] = true;
+				break;
+
+			case ALLEGRO_KEY_DOWN:
+				key[KEY_DOWN] = true;
+				break;
+
+			case ALLEGRO_KEY_LEFT:
+				key[KEY_LEFT] = true;
+				break;
+
+			case ALLEGRO_KEY_RIGHT:
+				key[KEY_RIGHT] = true;
+				break;
+			}
+		}
+		else if (ev.type == ALLEGRO_EVENT_KEY_UP) {
+			switch (ev.keyboard.keycode) {
+			case ALLEGRO_KEY_UP:
+				key[KEY_UP] = false;
+				break;
+
+			case ALLEGRO_KEY_DOWN:
+				key[KEY_DOWN] = false;
+				break;
+
+			case ALLEGRO_KEY_LEFT:
+				key[KEY_LEFT] = false;
+				break;
+
+			case ALLEGRO_KEY_RIGHT:
+				key[KEY_RIGHT] = false;
+				break;
+
+			case ALLEGRO_KEY_ESCAPE:
+				doexit = true;
+				break;
+			}
+		}
+
+		if (redraw && al_is_event_queue_empty(event_queue)) {
+			redraw = false;
+
+			al_clear_to_color(al_map_rgb(0, 0, 0));
+
+			al_draw_bitmap(player, player_x, player_y, 0);
+
+			al_flip_display();
+		}
+	}
+
 	al_destroy_bitmap(player);
+	al_destroy_timer(timer);
+	al_destroy_display(display);
+	al_destroy_event_queue(event_queue);
 
 	return 0;
 }
